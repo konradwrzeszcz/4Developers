a56b80279659f6fbde1eae063f4fe5bdf59ae710
konwrz20 <konwrz20@gmail.com>
2021-05-20 23:01:31 +0200

    more images

--- TravelAgency/DeclarativeCode/ServiceConfigurator.cs
+++ TravelAgency/DeclarativeCode/ServiceConfigurator.cs
@@ -1,6 +1,5 @@
 using System;
 using Microsoft.Extensions.DependencyInjection;
-using static TravelAgency.DeclarativeCode.GetUtcNowDecorator;
 
 namespace TravelAgency.DeclarativeCode {
     public static class ServiceConfigurator {
